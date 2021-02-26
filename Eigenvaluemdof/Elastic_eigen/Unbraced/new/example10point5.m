6b3a70f9449790c01eeff6dba3398aea5ea0e8e8
SamiUllah Khan Bangash <79264230+SamiUllahKBangash@users.noreply.github.com>
2021-02-26 20:34:39 +0500

    Update example10point5.m

--- Eigenvaluemdof/Elastic_eigen/Unbraced/new/example10point5.m
+++ Eigenvaluemdof/Elastic_eigen/Unbraced/new/example10point5.m
@@ -1,6 +1,6 @@
 function [nodes,StoreyNodes,elems,bcs,bcs_unbraced,loads]=example10point5
     
-
+% xcoord ycoord
 nodes=[0.0 	0.0;
         0   288;
         240	288;
@@ -8,17 +8,17 @@ nodes=[0.0 	0.0;
         720  0;
         240  0];
     
-StoreyNodes=[1 6 5;
-             2 3 4];    
+StoreyNodes=[1 6 5;   %ground storey nodes
+             2 3 4];  %(ground +1) storey nodes ...   
 
-                          %(Node1 Node2), E, I, A,Fy,Zx
+      %(Node1 Node2), E, I, A,Fy,Zx
 elems= [1 2 29000 248 13.3 36 54.9;
         6 3 29000 248 13.3 36 54.9;
         5 4 29000 248 13.3 36 54.9;
         2 3 29000 2850 24.8 36 244;
         3 4 29000 2850 24.8 36 244];
 
-                       % (Node  dof  disp)
+% (Node dof disp)
 bcs=[1 1 0;
      1 2 0;
      1 3 0;
@@ -28,7 +28,8 @@ bcs=[1 1 0;
      5 1 0;
      5 2 0;
      5 3 0];
-% (Node Storey dir  val)
+     
+% (Node Storey dof  disp)
  bcs_unbraced=[1 1 1 0;
                1 1 2 0;
                1 1 3 0;
@@ -39,7 +40,7 @@ bcs=[1 1 0;
                5 1 2 0;
                5 1 3 0];
 
-                  % (Node  dof  load)
+% (Node dof load)
 loads=[2 2 -1;
        3 2 -1;
        4 2 -1];
