e91abb24654f600e79bbe8ca5a4dee297a415bf8
SamiUllah Khan Bangash <79264230+SamiUllahKBangash@users.noreply.github.com>
2021-02-27 00:43:52 +0500

    Update normal_projection.m

--- secondorderinelastic/normal_projection.m
+++ secondorderinelastic/normal_projection.m
@@ -7,62 +7,6 @@ function [msol,psol,alphasol]=normal_projection(m1,p1,Py,Mp)
 %previously. Matlab's vpasolve is invoked to solve three nonlinear
 %equations for m, p, alpha parameters. 
 
-
-
-%{
-m1=0.4;
-p1=0.3;
-Py=955; %kips/K
-Mp=4840; %Kin
-mi=sign(m1)*0.5;
-converge=false;
-j=1;
-while ~converge && j<=100
-    
-    if p1>0
-        m=((2+7*mi^2)*Mp*(m1 + 3.5*m1*mi^2 - mi + 3.5*mi^3))/(9*Py*(p1*sqrt(1+3.5*mi^2) - sqrt(1-mi^2)));
-    else
-        m=((2+7*mi^2)*Mp*(m1 + 3.5*m1*mi^2 - mi + 3.5*mi^3))/(9*Py*(p1*sqrt(1+3.5*mi^2) - sqrt(1-mi^2)));
-    end
-    
-    if (abs(m-mi)/mi)*100 <=0.01
-        converge=true;
-        j=j+1;
-    else
-        mi=m;
-        j=j+1;
-    end
-end
-%}
-%{
-m1=0.87;
-p1=0.4;
-Py=955; %kips/K
-Mp=4840; %Kin
-%}
-%{
-syms m real
-
-if p1>0
-y1=(((1-m.^2)./(1+3.5*m.^2)).^0.5);
-else
-y1=-(((1-m.^2)./(1+3.5*m.^2)).^0.5); 
-end
-
-y2=(Mp*(m1-m).*(1+3.5*m.^2).*(2+7*m.^2));
-y3=Py*(2*m.*(1+3.5*m.^2) + 7*m.*(1-m.^2));
-yfinal=y1.*(1 + y2./y3) - p1;
-[mcorrec]=vpasolve(yfinal==0,m,[0 1]);
-if p1>0
-   pcorrec=sqrt((1-mcorrec^2)/(1+3.5*mcorrec^2));
-else
-   pcorrec=-sqrt((1-mcorrec^2)/(1+3.5*mcorrec^2)); 
-end
-%mcorrec;
-%pcorrec;
-phi=mcorrec^2 + pcorrec^2 + 3.5*(mcorrec*pcorrec)^2;
-%}
-      
 syms m p alp real
 eq1=m*(2*alp + 7*alp*p^2 + Mp)== m1*Mp;
 
