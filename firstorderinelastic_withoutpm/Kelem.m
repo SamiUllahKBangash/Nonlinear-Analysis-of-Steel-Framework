cfb1871fc75b7a1d71f34eef6430916dec53fd26
SamiUllah Khan Bangash <79264230+SamiUllahKBangash@users.noreply.github.com>
2021-02-26 22:51:13 +0500

    Update Kelem.m

--- firstorderinelastic_withoutpm/Kelem.m
+++ firstorderinelastic_withoutpm/Kelem.m
@@ -11,8 +11,7 @@ E1 = [ (nodexy(2,1)-nodexy(1,1))  (nodexy(2,2)-nodexy(1,2)) ];
 L = norm(E1);
 E1 = E1/L;
 E2 = [-E1(2) E1(1)];
-if abs(mp-abs(mi))<0.01*mp && abs(mp-abs(mj))>0.01*mp
-%if abs(mi)>=0.99*mp && abs(mj)<0.99*mp    
+if abs(mp-abs(mi))<0.01*mp && abs(mp-abs(mj))>0.01*mp 
     
      k_1=rho*e*i/L*[ a/i 0 0;     %perfectly elastic portion
                     0    4 2;
@@ -22,7 +21,7 @@ if abs(mp-abs(mi))<0.01*mp && abs(mp-abs(mj))>0.01*mp
                         0    0 3];
     
 elseif  abs(mp-abs(mi))>0.01*mp && abs(mp-abs(mj))<0.01*mp
-%elseif abs(mi)<0.99*mp && abs(mj)>=0.99*mp    
+
      k_1=rho*e*i/L*[ a/i 0 0;     %perfectly elastic portion
                     0    4 2;
                     0    2 4];
@@ -30,7 +29,7 @@ elseif  abs(mp-abs(mi))>0.01*mp && abs(mp-abs(mj))<0.01*mp
                         0    3 0;
                         0    0 0];
 elseif  abs(mp-abs(mi))<0.01*mp && abs(mp-abs(mj))<0.01*mp
-%elseif abs(mi)>=0.99*mp && abs(mj)>=0.99*mp    
+  
      k_1=rho*e*i/L*[ a/i 0 0;     %perfectly elastic portion
                     0    4 2;
                     0    2 4];
@@ -47,27 +46,14 @@ else
                         0    2 4];
 end
     
- %{    
-Kel_LOC1 = [ A*E/L       0         0   -A*E/L         0              0;
-            0       3*E*I/(L^3)   0     0       -3*E*I/(L^3)   3*E*I/(L^2); 
-            0           0         0     0            0              0;
-           -A*E/L       0         0   A*E/L          0              0;
-            0      -3*E*I/(L^3)   0     0       3*E*I/(L^3)    -3*E*I/(L^2);
-        	0       3*E*I/(L^2)   0     0      -3*E*I/(L^2)     3*E*I/L ];
-        
-Kel_LOC2 = 0.00001*[ A*E/L    0   0   -A*E/L  0   0;
-        0   12*E*I/(L^3)    6*E*I/(L^2)   0    -12*E*I/(L^3)   6*E*I/(L^2); 
-        0   6*E*I/(L^2)     4*E*I/L      0    -6*E*I/(L^2)    2*E*I/L;
-        -A*E/L    0   0   A*E/L  0   0;
-        0   -12*E*I/(L^3)   -6*E*I/(L^2)  0    12*E*I/(L^3)    -6*E*I/(L^2);
-        0   6*E*I/(L^2)     2*E*I/L    0     -6*E*I/(L^2)    4*E*I/L ];
-%}
+ 
 k=k_1 + k_2;
-at=[1  0   0 -1  0    0;             %element transformation matrix
+at=[1  0   0 -1  0    0;             %element corotational transformation matrix
     0 1/L 1 0 -1/L 0;
     0 1/L 0 0 -1/L 1];
 Qrot = [E1 ; E2];
 Qrot(3,3) = 1;
-Tmatrix = [Qrot zeros(3); zeros(3) Qrot];
+Tmatrix = [Qrot zeros(3); zeros(3) Qrot]; %element coordinate transformation matrix
+
 Kel = Tmatrix'*(at'*k*at)*Tmatrix;
 end
